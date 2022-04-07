2df423425d702a0ed09897c0e0db45b8cec5b792
nljshoxbb <nljshoxbb@gmail.com>
2022-04-08 01:14:13 +0800

    build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 
 # 获取主机ip,写入docker-compose配置，在容器中使用
-IP_ADDRESS=`ifconfig eth0 |ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}'`
+IP_ADDRESS=`ip route get 1 | awk '{print $NF;exit}'`
 
-sed -i "" "s/REPLACE_EXTERNAL_IP/${IP_ADDRESS}/g" docker-compose.yml
+sed -i  "s/REPLACE_EXTERNAL_IP/${IP_ADDRESS}/g" docker-compose.yml
 
